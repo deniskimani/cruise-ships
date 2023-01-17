a43375e18e3171e99a7fd6bdbde9d4718e02cf65
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-01-17 15:55:11 +0300

    Removed redundant code from port test suite

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -2,28 +2,38 @@ const Port = require("../src/Port.js")
 
 
 describe("Port", () => {
+    describe("", () => {
+        let port
+
+        beforeEach(() => {
+            port = new Port('Dover');
+
+        });
+
+        it("can add a ship", () => {
+
+            const ship = {}
+
+            port.addShip(ship)
+            expect(port.ships).toContain(ship)
+        })
+
+        it('can remove a ship', () => {
+
+            const titanic = {}
+            const queenMary = {}
+
+            port.addShip(titanic)
+            port.addShip(queenMary)
+            port.removeShip(queenMary)
+
+            expect(port.ships).toEqual([titanic])
+        })
+    })
     it("can be instatiated", () => {
         expect(new Port()).toBeInstanceOf(Object)
     })
 
-    it("can add a ship", () => {
-        const port = new Port('Dover')
-        const ship = {}
 
-        port.addShip(ship)
-        expect(port.ships).toContain(ship)
-    })
-
-    it('can remove a ship', () => {
-        const port = new Port('Dover')
-        const titanic = {}
-        const queenMary = {}
-
-        port.addShip(titanic)
-        port.addShip(queenMary)
-        port.removeShip(queenMary)
-
-        expect(port.ships).toEqual([titanic])
-    })
 
 })
