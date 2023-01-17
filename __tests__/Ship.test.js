a8c6fa8a136c3180d3aaca9e61077cd88777dc78
deniskimani <48308527+deniskimani@users.noreply.github.com>
2023-01-17 15:47:52 +0300

    Removed redundant code in Ship.test.js nest describe's callback functions

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -2,35 +2,45 @@ const Ship = require('../src/Ship.js')
 const Port = require("../src/Port.js")
 const Itinerary = require('../src/Itinerary')
 
-describe("Ship", () => {
-    it("can be instantiated", () => {
+describe('Ship', () => {
+    describe("with ports and an itinerary", () => {
+        let ship;
+        let dover;
+        let calais;
+        let itinerary;
 
-        const port = new Port('Dover')
-        const itinerary = new Itinerary([port])
-        const ship = new Ship(itinerary)
+        beforeEach(() => {
+            dover = new Port('Dover');
+            calais = new Port('Calais');
+            itinerary = new Itinerary([dover, calais]);
+            ship = new Ship(itinerary);
+        });
 
-        expect(ship).toBeInstanceOf(Object)
-    })
+        it("can be instantiated", () => {
+            expect(ship).toBeInstanceOf(Object)
+        })
 
-    it("has a starting port", () => {
+        it("has a starting port", () => {
 
-        const port = new Port("Dover")
-        const itinerary = new Itinerary([port])
-        const ship = new Ship(itinerary)
+            expect(ship.currentPort).toBe(dover, calais)
 
-        expect(ship.currentPort).toBe(port)
-    })
+        })
 
-    it('can set sail', () => {
-        const dover = new Port('Dover');
-        const calais = new Port('Calais');
-        const itinerary = new Itinerary([dover, calais]);
-        const ship = new Ship(itinerary);
+        it('can set sail', () => {
 
-        ship.setSail();
+            ship.setSail();
 
-        expect(ship.currentPort).toBeFalsy();
-        expect(dover.ships).not.toContain(ship);
+            expect(ship.currentPort).toBeFalsy();
+            expect(dover.ships).not.toContain(ship);
+
+        })
+
+        it('gets added to port on instantiation', () => {
+
+            expect(dover.ships).toContain(ship);
+
+        });
+
     })
 
     it("can dock at a different port", () => {
@@ -56,13 +66,4 @@ describe("Ship", () => {
 
         expect(() => ship.setSail()).toThrowError('End of itinerary reached');
     })
-
-    it('gets added to port on instantiation', () => {
-        const dover = new Port('Dover');
-        const itinerary = new Itinerary([dover]);
-        const ship = new Ship(itinerary);
-
-        expect(dover.ships).toContain(ship);
-    });
-
 })
